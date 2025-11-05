697602a05bd5c51e887d0a92e7bcc565a1d2e471
djxei <409059@niuitmo.ru>
2025-11-05 03:01:35 +0300

    timeout 3s

--- LLVM_Pass_hw2/get_traceLog.sh
+++ LLVM_Pass_hw2/get_traceLog.sh
@@ -6,6 +6,6 @@ mkdir -p tmp
 
 clang -O1 -fpass-plugin=./libPass.so -c ../graphic_app_hw1/app.c -o tmp/app.o
 
-clang log.c tmp/app.o ../graphic_app_hw1/sim.c ../graphic_app_hw1/start.c -lSDL2 -o tmp/a.out && timeout 1s ./tmp/a.out 
+clang log.c tmp/app.o ../graphic_app_hw1/sim.c ../graphic_app_hw1/start.c -lSDL2 -o tmp/a.out && timeout 3s ./tmp/a.out 
 
 rm -rf tmp
